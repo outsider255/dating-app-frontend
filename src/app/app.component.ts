1e6123a472cf1936540bae6efab489e91004473f
Damian Wojciechowski <outsider255@gmail.com>
2023-05-23 20:19:26 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'dating app';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7261/users').subscribe({
+next: (request) => this.users=request,
+error: (error) => console.log(error),
+complete: ()=> {console.log('Request finished')}
+  }
+)
+}
 }
